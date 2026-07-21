644bc5443397f08cf96602a46c1fe7ebc3e3a051
manar_alramadan <manaralramadan98@gmail.com>
2026-07-21 15:10:14 +0300

    feat: implement admin authentication backend

---
+++ backend/src/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const adminRoutes = require("./routes/adminRoutes");
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+connectDB();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+// Admin API
+app.use("/api/admin", adminRoutes);
+
+
+
+app.get("/", (req,res)=>{
+    res.json({
+        message:"PlaceFire API Running"
+    });
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
